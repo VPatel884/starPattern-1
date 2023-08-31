f3985d8775f74f6f05c799b9d6b1be9ccf043781
VPatel884 <vpatel884@gmail.com>
2023-08-31 17:28:38 +0000

    Code to print diamond shape

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ let readlineSync = require('readline-sync');
 
 let n = readlineSync.question("Enter number of rows: ")
 
-for (let i=1; i<=n; i++) {
-  
+for (let i = 1; i <= n; i++) {
+
   let spc = "";
-  for (let j=1; j<=(n-i); j++) {
+  for (let j = 1; j <= (n - i); j++) {
     spc += " ";
   }
   let str = "";
-  for (let j=1; j<=((2*i)-1); j++) {
+  for (let j = 1; j <= ((2 * i) - 1); j++) {
     str += "*";
   }
-  
-  console.log(spc+str)
+
+  console.log(spc + str)
 }
 
-for (let i=n; i>=1; i--) {
-  
+for (let i = n; i >= 1; i--) {
+
   let spc1 = "";
-  for (let j=1; j<=(n-i); j++) {
+  for (let j = 1; j <= (n - i); j++) {
     spc1 += " ";
   }
   let str1 = "";
-  for (let j=1; j<=((2*i)-1); j++) {
+  for (let j = 1; j <= ((2 * i) - 1); j++) {
     str1 += "*";
   }
-  
-  console.log(spc1+str1)
+
+  console.log(spc1 + str1)
 }
